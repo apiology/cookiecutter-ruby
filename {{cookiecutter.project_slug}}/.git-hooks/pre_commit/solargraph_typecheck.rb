e1ca6f7cc9b8ac4d59dbeb682c350622613b1694
Vince Broz <vince@broz.cc>
2023-05-13 23:28:25 -0400

    Add typechecking with solargraph

---
+++ {{cookiecutter.project_slug}}/.git-hooks/pre_commit/solargraph_typecheck.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'overcommit'
+
+# @!override Overcommit::Hook::Base#execute
+#   @return [Overcommit::Subprocess::Result]
+
+module Overcommit
+  module Hook
+    module PreCommit
+      # Runs `solargraph typecheck` against any modified Ruby files.
+      class SolargraphTypecheck < Base
+        def run
+          errors = []
+
+          applicable_files.each do |file|
+            generate_errors_for_file(file, errors)
+          end
+
+          # output message to stderr
+          errors
+        end
+
+        private
+
+        def generate_errors_for_file(file, errors)
+          file_dir = File.dirname(file)
+
+          # Change into the directory of the file before running solargraph
+          Dir.chdir(file_dir) do
+            result = execute(['bundle', 'exec', 'solargraph', 'typecheck', '--level', 'strict', file])
+            return if result.success?
+
+            # @type [String]
+            stdout = result.stdout
+
+            stdout.split("\n").each do |error|
+              error = parse_error(file, error)
+              errors << error unless error.nil?
+            end
+          end
+        end
+
+        def parse_error(file, error)
+          # Parse the result for the line number                # @type [MatchData]
+          match = error.match(/^(.+?):(\d+)/)
+          return nil unless match
+
+          # @!override MatchData.captures
+          #   @return [Array]
+          # @sg-ignore
+          file_path, lineno = match.captures
+          message = error.sub("#{file_path}:#{lineno} - ",
+                              "#{file_path}:#{lineno}: ")
+          # Emit the errors in the specified format
+          Overcommit::Hook::Message.new(:error, file, lineno, message)
+        end
+      end
+    end
+  end
+end
