00e0780f2b2994a34c8964e2823050cfca5dcf7a
Vince Broz <vince@broz.cc>
2024-10-30 07:31:49 -0400

    Update template

--- Gemfile
+++ Gemfile
@@ -4,5 +4,5 @@ source 'https://rubygems.org'
 
 gem 'mdl'
 gem 'overcommit', ['>=0.64.0', '<0.65.0']
-gem 'punchlist'
+gem 'punchlist', ['>=1.3.1']
 gem 'ostruct'
--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,14 @@ import subprocess
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
+def run(*args, **kwargs):
+    if len(kwargs) > 0:
+        print('running with kwargs', kwargs, ":", *args)
+    else:
+        print('running', *args)
+    subprocess.check_call(*args, **kwargs)
+
+
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
@@ -15,7 +23,7 @@ if __name__ == '__main__':
         remove_file('LICENSE')
         remove_file('CONTRIBUTING.md')
 
-    subprocess.check_call('./fix.sh')
+    run('./fix.sh')
     if os.environ.get('IN_COOKIECUTTER_PROJECT_UPGRADER', '0') == '1':
         os.environ['SKIP_GIT_CREATION'] = '1'
         os.environ['SKIP_GITHUB_AND_CIRCLECI_CREATION'] = '1'
@@ -24,12 +32,12 @@ if __name__ == '__main__':
         # Don't run these non-idempotent things when in
         # cookiecutter_project_upgrader, which will run this hook
         # multiple times over its lifetime.
-        subprocess.check_call(['git', 'init'])
-        subprocess.check_call(['git', 'add', '-A'])
-        subprocess.check_call(['bundle', 'exec', 'overcommit', '--install'])
-        subprocess.check_call(['bundle', 'exec', 'overcommit', '--sign'])
-        subprocess.check_call(['bundle', 'exec', 'overcommit', '--sign', 'pre-commit'])
-        subprocess.check_call(['bundle', 'exec', 'git', 'commit', '-m',
+        run(['git', 'init'])
+        run(['git', 'add', '-A'])
+        run(['bundle', 'exec', 'overcommit', '--install'])
+        run(['bundle', 'exec', 'overcommit', '--sign'])
+        run(['bundle', 'exec', 'overcommit', '--sign', 'pre-commit'])
+        run(['bundle', 'exec', 'git', 'commit', '-m',
                                'Initial commit from boilerplate'])
 
     if os.environ.get('SKIP_GITHUB_AND_CIRCLECI_CREATION', '0') != '1':
@@ -43,18 +51,18 @@ if __name__ == '__main__':
                                    'cookiecutter.type_of_github_repo: '
                                    '{{ cookiecutter.type_of_github_repo }}')
             description = "{{ cookiecutter.project_short_description.replace('\"', '\\\"') }}"
-            subprocess.check_call(['gh', 'repo', 'create',
-                                   visibility_flag,
-                                   '--description',
-                                   description,
-                                   '--source',
-                                   '.',
-                                   '{{ cookiecutter.github_username }}/'
-                                   '{{ cookiecutter.project_slug }}'])
-            subprocess.check_call(['gh', 'repo', 'edit',
-                                   '--allow-update-branch',
-                                   '--enable-auto-merge',
-                                   '--delete-branch-on-merge'])
-            subprocess.check_call(['git', 'push'])
-            subprocess.check_call(['circleci', 'follow'])
-            subprocess.check_call(['git', 'branch', '--set-upstream-to=origin/main', 'main'])
+            run(['gh', 'repo', 'create',
+                 visibility_flag,
+                 '--description',
+                 description,
+                 '--source',
+                 '.',
+                 '{{ cookiecutter.github_username }}/'
+                 '{{ cookiecutter.project_slug }}'])
+            run(['gh', 'repo', 'edit',
+                 '--allow-update-branch',
+                 '--enable-auto-merge',
+                 '--delete-branch-on-merge'])
+            run(['git', 'push'])
+            run(['circleci', 'follow'])
+            run(['git', 'branch', '--set-upstream-to=origin/main', 'main'])
