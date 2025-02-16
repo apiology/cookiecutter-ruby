22ebf7e0acc3ef378a12de83138da0f3621c8b86
Vince Broz <vince@broz.cc>
2025-02-15 22:40:07 -0500

    Improve Sorbet annotations

--- {{cookiecutter.project_slug}}/config/annotations_sorbet.rb
+++ {{cookiecutter.project_slug}}/config/annotations_sorbet.rb
@@ -7,9 +7,14 @@ require 'sorbet-runtime'
 #     class Struct
 #       extend T::Props::ClassMethods
 #     end
-#     # @param type [T::Types::Base,Class<Object>]
+#     # @param type [T::Types::Base,::Class<Object>]
 #     # @return [T::Types::Base]
 #     def self.nilable(type)
+#     end
+#     # @param obj [BasicObject]
+#     # @param type [T::Types::Base,::Class<Object>]
+#     # @return [BasicObject]
+#     def self.let(obj, type)
 #     end
 #     # @return [T::Types::Base]
 #     def self.untyped
