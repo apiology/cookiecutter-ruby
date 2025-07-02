460f8c65ce962641d77855203f9ab4918cc321cf
Vince Broz <vince@broz.cc>
2025-07-02 08:20:49 -0400

    reformat

--- {{cookiecutter.project_slug}}/.git-hooks/pre_commit/circle_ci.rb
+++ {{cookiecutter.project_slug}}/.git-hooks/pre_commit/circle_ci.rb
@@ -1,3 +1,4 @@
+# typed: true
 # frozen_string_literal: true
 
 require 'overcommit'
@@ -8,13 +9,17 @@ module Overcommit
     module PreCommit
       # CircleCI plugin for Overcommit to validate config file (.circleci/config.yml)
       class CircleCi < Base
+        # @return [Symbol, Array<Symbol, String>]
         def run
           result = execute(command)
+          # @sg-ignore
           return :pass if result.success?
 
+          # @sg-ignore
           if result.success?
             :pass
           else
+            # @sg-ignore
             [:fail, result.stderr]
           end
         end
